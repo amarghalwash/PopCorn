9fd488239d0144e7f01e4c631560c71582ada934
AmAr <amar.glwsh28@gmail.com>
2025-01-06 03:17:57 +0200

    Second commit

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -2,11 +2,21 @@ export default function Footer() {
   return (
     <footer className="footer">
       Created by{" "}
-      <a href="https://www.linkedin.com/in/amar-ghalwash-80aa70277/">
+      <a
+        href="https://www.linkedin.com/in/amar-ghalwash-80aa70277/"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
         Ammar Ghalwash
       </a>{" "}
       under the supervision of{" "}
-      <a href="mailto:info@yourcompany.com">Jonas Schmedtmann</a>
+      <a
+        href="mailto:info@yourcompany.com"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        Jonas Schmedtmann
+      </a>
     </footer>
   );
 }
